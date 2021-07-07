b012d0c2a55aabcc0ac33a1d02ec3cd1b5501089
GajendranParthasarathi <72959740+GajendranParthasarathi@users.noreply.github.com>
2021-07-07 17:21:27 +0530

    Create for_if_continue.ino

---
+++ for_if_continue.ino
@@ -0,0 +1,106 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+ if (Serial.available()>0)
+  {
+int a=Serial.read();
+delay(2000);
+int b=Serial.read();
+Serial.print("Value a: ");
+ Serial.println(a);
+ Serial.print("Value b: ");
+ Serial.println(b);
+ delay(2000);
+for(int i=(a-48);i<10;++i)
+{
+Serial.print("Iteration:");
+Serial.println(i);
+if (b==49)
+continue;
+Serial.println("b!=1");}
+}}
+
+/*if a=1, b=1
+Value a: 49
+Value b: 49
+Iteration:1
+Iteration:2
+Iteration:3
+Iteration:4
+Iteration:5
+Iteration:6
+Iteration:7
+Iteration:8
+Iteration:9
+
+
+
+if a=0, b=1
+Value a: 48
+Value b: 49
+Iteration:0
+Iteration:1
+Iteration:2
+Iteration:3
+Iteration:4
+Iteration:5
+Iteration:6
+Iteration:7
+Iteration:8
+Iteration:9
+
+
+
+if a=1, b=0
+Value a: 49
+Value b: 48
+Iteration:1
+b!=1
+Iteration:2
+b!=1
+Iteration:3
+b!=1
+Iteration:4
+b!=1
+Iteration:5
+b!=1
+Iteration:6
+b!=1
+Iteration:7
+b!=1
+Iteration:8
+b!=1
+Iteration:9
+b!=1
+
+
+
+if a=0, b=0
+Value a: 48
+Value b: 48
+Iteration:0
+b!=1
+Iteration:1
+b!=1
+Iteration:2
+b!=1
+Iteration:3
+b!=1
+Iteration:4
+b!=1
+Iteration:5
+b!=1
+Iteration:6
+b!=1
+Iteration:7
+b!=1
+Iteration:8
+b!=1
+Iteration:9
+b!=1
+
+
+*/
